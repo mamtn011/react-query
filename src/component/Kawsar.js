c6d2aaa3f9c9a722911bd71ae0a882771f377687
Kawsar <web.kawsarahmed@gmail.com>
2023-08-23 19:59:12 +0600

    added crud UI for todolist

--- src/component/Kawsar.js
+++ src/component/Kawsar.js
@@ -1,3 +1,38 @@
+import { Button, Container, Table } from "react-bootstrap";
+import friends from "../utils/fakeData";
+
 export default function Kawsar() {
-  return <div>Kawser</div>;
+  return (
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {friends.map((friend) => (
+            <>
+              <tr keys={friend.id}>
+                <td>{friend.id}</td>
+                <td>{friend.firstName}</td>
+                <td>{friend.lastName}</td>
+                <td>{friend.userName}</td>
+                <td>{friend.email}</td>
+                <td>
+                  <Button variant="primary" style={{marginRight: '25px'}}>Edit</Button>
+                  <Button variant="danger">Delete</Button>
+                </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 }
---
+++ src/utils/fakeData.js
@@ -0,0 +1,27 @@
+
+
+const friends = [
+    {
+        id: 1,
+        firstName: 'Kawsar',
+        lastName: 'Ahmed',
+        userName: 'webKawsar',
+        email: 'kawsarahmed.dev@gmail.com'
+    },
+    {
+        id: 2,
+        firstName: 'Imran',
+        lastName: 'Ahmed',
+        userName: 'imran',
+        email: 'imran@gmail.com'
+    },
+    {
+        id: 3,
+        firstName: 'Ajel',
+        lastName: 'Miah',
+        userName: 'ajel',
+        email: 'ajel@gmail.com'
+    },
+
+]
+export default friends;
